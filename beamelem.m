23776c602320a7cfa9138ec99760614419747d79
dantan123 <40445343+dantan123@users.noreply.github.com>
2020-07-24 16:28:34 -0700

    Update beamelem.m

--- beamelem.m
+++ beamelem.m
@@ -6,7 +6,7 @@ k = EI / L ^ 3;    % constant coefficient for each truss element
 
 
 
-Ke = k*[12 , 6*L  ,  -12,   6*L;       % stiffness matrix for beam element
+Ke = k * [12 , 6*L  ,  -12,   6*L;       % stiffness matrix for beam element
         6*L, 4*L^2, -6*L, 2*L^2;
         -12,  -6*L,   12,  -6*L;
         6*L, 2*L^2, -6*L, 4*L^2];
